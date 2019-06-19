10a4e7105fc752812c229ed8c687093eb3237a98
mome <diablo3foracount@gmail.com>
2019-06-19 20:25:46 +0900

    ディスコードID追加

--- db/migrate/20190618134125_create_users.rb
+++ db/migrate/20190618134125_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.integer :rate
-
+      t.integer :discord_id
       t.timestamps
     end
   end
